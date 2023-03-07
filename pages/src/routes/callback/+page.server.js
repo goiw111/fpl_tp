ac8874ed7d39fbd6144ae2efca6669ca04170e06
sohayl <chahmot.s@gmail.com>
2023-03-07 23:24:36 +0100

    debug

--- pages/src/routes/callback/+page.server.js
+++ pages/src/routes/callback/+page.server.js
@@ -4,13 +4,12 @@ const target  = 'https://oauth2.googleapis.com/token'
 
 export async function load({ url ,fetch ,platform }) {
   const code          = url.searchParams.get('code')
-  console.log(code)
   const client_id     = '1057658084984-oku26m2qmlrhbifmq6t2ocbd2cgda3ll.apps.googleusercontent.com'
   const client_secret = CLIENT_SECRET
   const grant_type    = 'authorization_code'
   const redirect_uri  = import.meta.env.VITE_REDIRECT_URL 
-  console.log(redirect_uri)
-  const data = new URLSearchParams({
+
+  const body = new URLSearchParams({
     code,
     client_id,
     client_secret,
@@ -18,14 +17,9 @@ export async function load({ url ,fetch ,platform }) {
     redirect_uri,
   })
 
-  const body = decodeURI(data.toString())
-
-  console.log(body)
-
   const response = await fetch(target,{
     method: "POST",
     Headers: {
-      Accept: 'application/json',
       'Content-Type': 'application/x-www-form-urlencoded'
     },
     body
